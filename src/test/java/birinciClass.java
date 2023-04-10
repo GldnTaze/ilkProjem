13f8ea65a1304a4235e1d9639fd4d92f5a8c9665
Tazeoglu <guldentaze2706@gmail.com>
2023-04-10 11:56:19 +0200

    ikinciCommit

---
+++ src/test/java/birinciClass.java
@@ -0,0 +1,10 @@
+public class birinciClass {
+    public static void main(String[] args) {
+        System.out.println("merhaba dunya");
+        for (int i = 0; i <5 ; i++) {
+            System.out.println(i);
+
+        }
+        System.out.println("yeni bir satir ekledim");
+    }
+}
